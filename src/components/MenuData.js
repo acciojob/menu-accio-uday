0f964d124cfa12b99881a8115f2ad3ba9349c459
accio-uday <udaymanideep2002@gmail.com>
2025-08-15 12:57:50 +0530

    Create MenuData.js

---
+++ src/components/MenuData.js
@@ -0,0 +1,46 @@
+const menuData = [
+  {
+    id: 1,
+    name: "Pancakes",
+    category: "Breakfast",
+    price: "$5",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 2,
+    name: "Omlette",
+    category: "Breakfast",
+    price: "$6",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 3,
+    name: "Burger",
+    category: "Lunch",
+    price: "$8",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 4,
+    name: "Grilled Cheese",
+    category: "Lunch",
+    price: "$7",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 5,
+    name: "Strawberry Shake",
+    category: "Shakes",
+    price: "$4",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 6,
+    name: "vanilla Shake",
+    category: "Shakes",
+    price: "$4",
+    image: "https://via.placeholder.com/100"
+  }
+];
+
+export default menuData;
